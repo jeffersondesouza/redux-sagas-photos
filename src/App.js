032b0aeb7cf291e31e3e272ed401662a0bbea4dc
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-03-05 18:23:09 -0300

    pages created

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 import Header from './components/Header';
 import ImageGrid from './components/ImageGrid/ImageGrid';
 
+import createBrowserHistory from 'history/createBrowserHistory';
 import configureStore from './store';
+
 const store = configureStore();
 
 class App extends Component {
